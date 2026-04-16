0dc68f7de92b55948875495107889def0fb64297
Your Name <you@example.com>
2026-04-16 19:13:11 +0500

    Fix build for static export

--- frontend-next/next.config.ts
+++ frontend-next/next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'standalone', // Standalone is preferred for Docker, but 'export' is causing crashes with dynamic routes.
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  turbopack: {
-    root: process.cwd(),
-  },
+  // turbopack: {
+  //   root: process.cwd(),
+  // },
 };
 
 export default nextConfig;
